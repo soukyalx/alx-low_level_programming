f7bf3ac8456f9e312d1fede3e6604b6fd84adbce
soukylax <soukainahardoum@gmail.com>
2023-07-13 09:27:17 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- *
+ * main - prints "A program that prints a line with puts function
  * Return : 0 (Success)
  */
 int main(void)
