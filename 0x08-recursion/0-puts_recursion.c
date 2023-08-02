c0855c7c682dece3441214a5e4576dc033d7a159
soukylax <soukainahardoum@gmail.com>
2023-08-02 07:08:16 -0700

    tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,13 @@
  *followed by a new line
  *@s : string to be printed
  */
-void
-_puts_recursion (char *s)
+void _puts_recursion(char *s)
 {
-  if (*s == '\0')
-    {
-      _putchar ('\n');
-      return;
-    }
-  _putchar (*s);
-  _puts_recursion (s + 1);
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+		return;
+	}
+	_putchar (*s);
+	_puts_recursion(s + 1);
 }
