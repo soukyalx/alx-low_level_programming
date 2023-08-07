8e06d1b15e998699d4e2cb5ae847ca1dfcac1bde
soukylax <soukainahardoum@gmail.com>
2023-08-07 01:13:26 -0700

    task2

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _putchar(char c);
 char *create_array(unsigned int size, char c);
+int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
