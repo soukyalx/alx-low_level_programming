a9822ebe233588900b4741d78ae7e437f4bfcd36
soukyalx <soukainahardoum@gmail.com>
2023-08-28 10:07:46 -0700

    tasks

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -11,11 +11,11 @@ int sum_listint(listint_t *head)
 	int sum = 0;
 	listint_t *temp = head;
 
-    while (temp)
-    {
-	    sum += temp->n;
-	    temp = temp->next;
-    }
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
 
-    return (sum);
+	return (sum);
 }
